f6e776d32f01c182c74527490afd50dd4e86f259
Kazeem Onisarotu <konisarotu@yahoo.com>
2018-03-24 16:49:28 +0100

    files deleted

--- farmhouse/public/js/create.js
+++
@@ -1,6 +0,0 @@
-var url = location.pathname.split('/')[3];
-$(document).ready(function(){
-	$('#dashboard').attr('href','/farmer/'+url);
-	$('#create').attr('href','/farmer/'+'create/'+url);
-	$('#owner').attr('value',url);
-});
